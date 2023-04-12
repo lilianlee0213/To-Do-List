46939c3184615cc245e39e6b9338dceeabfb5d60
Lilian Lee <lilianlee0213@gmail.com>
2023-04-12 12:31:32 -0400

    Set up

---
+++ src/styled.d.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import {DefaultTheme} from 'styled-components';
+declare module 'styled-components' {
+	export interface DefaultTheme {
+		bgColor: string;
+		textColor: string;
+		accentColor: string;
+		cardBgColor: string;
+	}
+}
