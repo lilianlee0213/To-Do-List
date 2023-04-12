baefb86310f21c52055f36761fbfc17bb5dbcd61
Lilian Lee <lilianlee0213@gmail.com>
2023-04-12 17:05:05 -0400

    selectors pt.2

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,39 +1,31 @@
-import {useRecoilValue} from 'recoil';
+import {useRecoilState, useRecoilValue} from 'recoil';
 import CreateToDo from './CreateToDo';
+import {categoryState, toDoSelector} from '../atoms';
 import ToDo from './ToDo';
-import {toDoSelector, toDoState} from '../atoms';
 
 interface IForm {
 	toDo: string;
 }
 
 function ToDoList() {
-	const [toDo, doing, done] = useRecoilValue(toDoSelector);
-
+	const toDos = useRecoilValue(toDoSelector);
+	const [category, setCategory] = useRecoilState(categoryState);
+	const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+		setCategory(event.currentTarget.value);
+	};
 	return (
 		<div>
-			<CreateToDo />
 			<h1>To Dos</h1>
-			<ul>
-				{toDo.map((toDo) => (
-					<ToDo key={toDo.id} {...toDo} />
-				))}
-			</ul>
 			<hr />
-			<h1>Doing</h1>
-			<ul>
-				{doing.map((toDo) => (
-					<ToDo key={toDo.id} {...toDo} />
-				))}
-			</ul>
-			<hr />
-			<h1>Done</h1>
-			<ul>
-				{done.map((toDo) => (
-					<ToDo key={toDo.id} {...toDo} />
-				))}
-			</ul>
-			<hr />
+			<select onInput={onInput}>
+				<option value="TO_DO">To Do</option>
+				<option value="DOING">Doing</option>
+				<option value="DONE">Done</option>
+			</select>
+			<CreateToDo />
+			{toDos?.map((toDo) => (
+				<ToDo key={toDo.id} {...toDo} />
+			))}
 		</div>
 	);
 }
